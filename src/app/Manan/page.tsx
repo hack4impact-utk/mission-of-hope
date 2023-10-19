816786f8da86e23b59c66669d15206d8e4919e64
Manan Patel <mananpatel6902@gmail.com>
2023-10-19 16:54:36 -0400

    #13 - Adding sample page

---
+++ src/app/Manan/page.tsx
@@ -0,0 +1,14 @@
+import Link from '@mui/material/Link';
+
+const myPage = () => {
+  return (
+    <>
+      <div>This is a very cool page</div>
+      <div>
+        <Link href="/">Take me back to home page pls!!!!</Link>
+      </div>
+    </>
+  );
+};
+
+export default myPage;
