254e4b5538a1b1253e26a48b296710bf9dc3f432
Ty Anderson <tyanderson@Tys-MacBook-Pro.local>
2023-10-25 19:01:42 -0400

    link to another page and logo

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,13 @@
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <>
+      <h1>
+        <a href="https://missionofhope.org/">Mission Of Hope</a>{' '}
+      </h1>
+      <img
+        src="https://cdn.openart.ai/stable_diffusion/3954196738cf8aea63f36e49db4be38eebbd6caf_2000x2000.webp"
+        alt="jesus"
+      ></img>
+    </>
+  );
 }
