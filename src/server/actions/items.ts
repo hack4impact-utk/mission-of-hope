3ab952c7d2f597fe9c233f7dbd707c2d1c595d8a
jMcMaho9 <jmcmaho9@vols.utk.edu>
2024-01-05 19:55:52 -0500

    API routing for item creation

---
+++ src/server/actions/items.ts
@@ -0,0 +1,14 @@
+import dbConnect from '@/utils/db-connect';
+import ItemSchema from '../models/items';
+import { CreateItemRequest, ItemResponse } from '@/types/items';
+import items from '../models/items';
+
+export async function createItem(
+  donation: CreateItemRequest
+): Promise<ItemResponse> {
+  await dbConnect();
+
+  const response: ItemResponse = await ItemSchema.create(items);
+
+  return response;
+}
