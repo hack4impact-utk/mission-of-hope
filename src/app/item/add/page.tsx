69450e9d87190e265db1bf49f5dcef5452037437
AlanKha <khalilialan@gmail.com>
2024-03-12 15:49:44 -0500

    Update styles.css for item add form

--- src/app/item/add/page.tsx
+++ src/app/item/add/page.tsx
@@ -19,160 +19,177 @@ const AddDonationForm = () => {
       <div className="vector"></div>
       <div className="form-container">
         <form>
-          <TextField
-            id="donorName"
-            label="Donor Name"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-
-          <TextField
-            type="date"
-            id="donationDate"
-            label="Donation Date"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-            InputLabelProps={{
-              shrink: true,
-            }}
-          />
-
-          <TextField
-            id="category"
-            label="Category"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-
-          <TextField
-            id="donatedItem"
-            label="Donated Item"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-
-          <TextField
-            id="itemValue"
-            label="Item Value"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-
-          <TextField
-            type="number"
-            id="quantity"
-            label="Quantity"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-
-          <TextField
-            id="donationValue"
-            label="Donation Value"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-
-          <FormControl fullWidth margin="normal" variant="outlined">
-            <InputLabel id="highOrLowValue-label">High or Low Value</InputLabel>
-            <Select
-              labelId="highOrLowValue-label"
-              id="highOrLowValue"
-              label="High or Low Value"
+          <div className="row1">
+            <TextField
+              id="donorName"
+              label="Donor Name"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              type="date"
+              id="donationDate"
+              label="Donation Date"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+              InputLabelProps={{
+                shrink: true,
+              }}
+            />
+          </div>
+          <div className="row2">
+            <TextField
+              id="category"
+              label="Category"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+          </div>
+          <div className="row3">
+            {' '}
+            <TextField
+              id="donatedItem"
+              label="Donated Item"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              type="number"
+              id="quantity"
+              label="Quantity"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+          </div>
+          <div className="row4">
+            <TextField
+              id="itemValue"
+              label="Item Value"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              id="donationValue"
+              label="Donation Value"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+            <FormControl fullWidth margin="normal" variant="outlined">
+              <InputLabel id="highOrLowValue-label">
+                High or Low Value
+              </InputLabel>
+              <Select
+                labelId="highOrLowValue-label"
+                id="highOrLowValue"
+                label="High or Low Value"
+              >
+                <MenuItem value="high">High</MenuItem>
+                <MenuItem value="low">Low</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
+          <div className="row5">
+            {' '}
+            <TextField
+              id="user"
+              label="User"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+          </div>
+          <div className="row6">
+            {' '}
+            <Button
+              type="submit"
+              variant="contained"
+              color="primary"
+              className="submit-button"
             >
-              <MenuItem value="high">High</MenuItem>
-              <MenuItem value="low">Low</MenuItem>
-            </Select>
-          </FormControl>
+              Add Donation
+            </Button>
+            <FormControl fullWidth margin="normal" variant="outlined">
+              <InputLabel id="hasDonatedBefore-label">
+                Has this donor previously donated?
+              </InputLabel>
+              <Select
+                labelId="hasDonatedBefore-label"
+                id="hasDonatedBefore"
+                label="Has this donor previously donated?"
+              >
+                <MenuItem value="yes">Yes</MenuItem>
+                <MenuItem value="no">No</MenuItem>
+              </Select>
+            </FormControl>
+          </div>
 
-          <TextField
-            id="user"
-            label="User"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
+          <div className="row7">
+            {' '}
+            <TextField
+              id="donorName"
+              label="Donor Name"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              type="email"
+              id="donorEmail"
+              label="Email"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+            <TextField
+              type="tel"
+              id="donorPhone"
+              label="Phone"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+          </div>
 
-          <FormControl fullWidth margin="normal" variant="outlined">
-            <InputLabel id="hasDonatedBefore-label">
-              Has this donor previously donated?
-            </InputLabel>
-            <Select
-              labelId="hasDonatedBefore-label"
-              id="hasDonatedBefore"
-              label="Has this donor previously donated?"
-            >
-              <MenuItem value="yes">Yes</MenuItem>
-              <MenuItem value="no">No</MenuItem>
-            </Select>
-          </FormControl>
+          <div className="row8">
+            <TextField
+              id="donorAddress"
+              label="Address"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
 
-          {/* Repeat the TextField pattern for the new donor fields */}
-          <TextField
-            type="email"
-            id="donorEmail"
-            label="Email"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
+            <TextField
+              id="donorCity"
+              label="City"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
 
-          <TextField
-            type="tel"
-            id="donorPhone"
-            label="Phone"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
+            <TextField
+              id="donorState"
+              label="State"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
 
-          <TextField
-            id="donorAddress"
-            label="Address"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-
-          <TextField
-            id="donorCity"
-            label="City"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-
-          <TextField
-            id="donorState"
-            label="State"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-
-          <TextField
-            id="donorZip"
-            label="Zip Code"
-            variant="outlined"
-            fullWidth
-            margin="normal"
-          />
-
-          <Button
-            type="submit"
-            variant="contained"
-            color="primary"
-            className="submit-button"
-          >
-            Add Donation
-          </Button>
+            <TextField
+              id="donorZip"
+              label="Zip Code"
+              variant="outlined"
+              fullWidth
+              margin="normal"
+            />
+          </div>
         </form>
       </div>
     </>
