55841328eb579dcf24047572bd19662a328c14dd
TrishuPatel <trishujpatel23@gmail.com>
2024-04-03 16:47:13 -0400

    Connect table to db

--- src/app/item/page.tsx
+++ src/app/item/page.tsx
@@ -1,3 +1,4 @@
+import { getAllItems } from '@/server/actions/items';
 import {
   Table,
   TableBody,
@@ -8,37 +9,7 @@ import {
 } from '@mui/material/';
 
 export default async function Home() {
-  const exItem1 = {
-    _id: {
-      $oid: '660b500bf64341a36558faee',
-    },
-    name: 'ItemY',
-    category: 'cheese',
-    high: 2,
-    low: 1,
-  };
-
-  const exItem2 = {
-    _id: {
-      $oid: '65fbb891b890681545364c2c',
-    },
-    name: 'ProductI',
-    category: 'Furniture',
-    high: 349.99,
-    low: 249.99,
-  };
-
-  const exItem3 = {
-    _id: {
-      $oid: '65fbb891b890681545364c26',
-    },
-    name: 'ProductC',
-    category: 'Home Decor',
-    high: 79.99,
-    low: 49.99,
-  };
-
-  const rows = [exItem1, exItem2, exItem3];
+  const rows = await getAllItems();
 
   const currency = new Intl.NumberFormat('en-US', {
     style: 'currency',
@@ -63,15 +34,21 @@ export default async function Home() {
         <TableBody>
           {rows &&
             rows.map((row) => (
-              <TableRow key={row._id.$oid}>
+              <TableRow key={row._id}>
                 <TableCell component="th" scope="row">
                   {row.name}
                 </TableCell>
                 <TableCell>{row.category}</TableCell>
-                <TableCell align="center">
-                  {currency.format(row.high)}
-                </TableCell>
-                <TableCell align="center">{currency.format(row.low)}</TableCell>
+                {row.high && (
+                  <TableCell align="center">
+                    {currency.format(row.high)}
+                  </TableCell>
+                )}
+                {row.low && (
+                  <TableCell align="center">
+                    {currency.format(row.low)}
+                  </TableCell>
+                )}
               </TableRow>
             ))}
         </TableBody>
