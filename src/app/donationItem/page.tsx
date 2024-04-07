82b19b61d637090c72ea829149b08be177c6aaa9
alay10 <alay10@vols.utk.edu>
2024-04-07 15:51:08 -0400

    Finally working

--- src/app/donationItem/page.tsx
+++ src/app/donationItem/page.tsx
@@ -2,20 +2,10 @@ import { DonationItemResponse } from '@/types/donation';
 import DonationItemAutofill from '@/components/donationItem';
 import { getAllDonationItems } from '@/server/actions/donationItem';
 
-// async function findDonationItems(): Promise<DonationItemResponse[] | null> {
-//   try {
-//     const data = await getAllDonationItems() ;
-//     return data;
-//   } catch (error) {
-//     console.log(error);
-//     return null;
-//   }
-// }
-
-// eslint-disable-next-line @next/next/no-async-client-component
 export default async function Home() {
-  const listOfDItems: DonationItemResponse[] = await getAllDonationItems();
-  // console.log(listOfDItems[0]);
+  const listOfDItems: DonationItemResponse[] = JSON.parse(
+    JSON.stringify(await getAllDonationItems())
+  );
 
   return (
     <DonationItemAutofill
@@ -24,11 +14,3 @@ export default async function Home() {
     />
   );
 }
-// try {
-//   if (listOfDItems !== null) {
-//   return <Card>Null</Card>;
-// } catch (error) {
-//   console.log('help');
-//   return <p>NULL</p>;
-// }
-// }
--- src/components/donationItem/index.tsx
+++ src/components/donationItem/index.tsx
@@ -1,8 +1,14 @@
 'use client'; //Needed for useState
-// import { Autocomplete, Card, CardContent, TextField, Typography } from '@mui/material';
+import {
+  Autocomplete,
+  Card,
+  CardContent,
+  TextField,
+  Typography,
+} from '@mui/material';
 // import { Card, CardContent, Typography } from '@mui/material';
 import { DonationItemResponse } from '@/types/donation';
-// import { useState } from 'react';
+import { useState } from 'react';
 
 interface Props {
   donationItems: DonationItemResponse[];
@@ -10,47 +16,60 @@ interface Props {
 }
 
 export default function DonationItemAutofill(props: Props) {
-  // const [donationItemOptions] = useState<DonationItemResponse[]>(
-  //   props.donationItems
-  // );
-  // const [donationItem /*, changeDonation*/] = useState<DonationItemResponse>(
-  //   props.donationItemData
-  // );
-  console.log(props.donationItems);
+  const [donationItemOptions] = useState<DonationItemResponse[]>(
+    props.donationItems
+  );
+  const [donationItem, changeDonation] = useState<DonationItemResponse>(
+    props.donationItemData
+  );
+  function onNameChange(value: string) {
+    const donMatch = donationItemOptions.filter(
+      (don) => don.item.name === value
+    );
+
+    if (donMatch.length === 1) {
+      changeDonation(donMatch[0]);
+    }
+  }
+  // I think there needs to be more error checking
   return (
     <>
-      <DonationItemTable
-        donationItemData={props.donationItemData}
-      ></DonationItemTable>
-      {/* <Autocomplete
-      sx={{ mt: 2 }}
-      freeSolo
-      autoComplete
-      value={donationItem.item.name || ''}
-      options={donationItemOptions}
-      getOptionLabel={(donIt) =>
-        typeof donIt === 'string' ? donIt : donIt.item.name
-      }
-      renderOption={(props, option) => {
-        return (
-          <li {...props} key={option._id}>
-            {option.item.name}
-          </li>
-        );
-      }}
-      renderInput={(params) => (
-        <TextField
-          {...params}
-          label="Email Address"
-          onChange={(e) => {
-            changeDonation({
-              ...props.donationItemData,
-              item: { ...props.donationItemData.item, name: e.target.value },
-            });
-          }}
-        />
-      )}
-    /> */}
+      <Autocomplete
+        sx={{ mt: 2 }}
+        freeSolo
+        autoComplete
+        value={donationItem.item.name || ''}
+        options={donationItemOptions}
+        isOptionEqualToValue={(options, value) => options._id === value._id}
+        getOptionLabel={(donIt) =>
+          typeof donIt === 'string' ? donIt : donIt.item.name
+        }
+        renderOption={(props, option) => {
+          return (
+            <li {...props} key={option._id}>
+              {option.item.name}
+            </li>
+          );
+        }}
+        renderInput={(params) => (
+          <TextField
+            {...params}
+            label="Email Address"
+            onChange={(e) => {
+              onNameChange(e.target.value);
+              // changeDonation({
+              //   ...props.donationItemData,
+              //   item: { ...props.donationItemData.item, name: e.target.value },
+              // });
+            }}
+          />
+        )}
+        onInputChange={(_, value) => {
+          onNameChange(value);
+          // changeDonation({...props.donationItemData, item: {...props.donationItemData.item, name: value}})
+        }}
+      />
+      <DonationItemTable donationItemData={donationItem}></DonationItemTable>
     </>
   );
 }
@@ -60,25 +79,18 @@ interface TableData {
 }
 
 function DonationItemTable(data: TableData) {
-  // const [dItemData, dItemChange] = useState<DonationItemResponse | null>(null);
-  // const listOfDItems: DonationItemResponse[] | null =
-  //   await findDonationItems();
-  // useEffect(() => dItemChange(listOfDItems[0]), [listOfDItems])
-
-  console.log(data);
   return (
-    <p>Hello</p>
-    // <Card>
-    //   <CardContent>
-    //     <Typography sx={{ fontWeight: 'bold' }} variant="h6" pt={2}>
-    //       {data.donationItemData.item.name}
-    //     </Typography>
-    //     <Typography>Quantity: {data.donationItemData.quantity}</Typography>
-    //     <Typography>Price: {data.donationItemData.value.price}</Typography>
-    //     <Typography>
-    //       Evaluation: {data.donationItemData.value.evaluation}
-    //     </Typography>
-    //   </CardContent>
-    // </Card>
+    <Card>
+      <CardContent>
+        <Typography sx={{ fontWeight: 'bold' }} variant="h6" pt={2}>
+          {data.donationItemData.item.name}
+        </Typography>
+        <Typography>Quantity: {data.donationItemData.quantity}</Typography>
+        <Typography>Price: {data.donationItemData.value.price}</Typography>
+        <Typography>
+          Evaluation: {data.donationItemData.value.evaluation}
+        </Typography>
+      </CardContent>
+    </Card>
   );
 }
