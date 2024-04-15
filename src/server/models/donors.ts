cad32ecac4466364a19a450889d0222423b44e9d
alay10 <alay10@vols.utk.edu>
2024-04-15 19:16:46 -0400

    Updated the schema to add city, state, zip

--- src/server/models/donors.ts
+++ src/server/models/donors.ts
@@ -19,6 +19,18 @@ const DonorSchema = new Schema(
       type: String,
       required: true,
     },
+    city: {
+      type: String,
+      required: true,
+    },
+    state: {
+      type: String,
+      required: true,
+    },
+    zip: {
+      type: Number,
+      required: true,
+    },
   },
   {
     versionKey: false,
