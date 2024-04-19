a6a61d482e304f587f45f7c3694b5fca3ecdfed6
Manan Patel <mananpatel6902@gmail.com>
2024-04-19 17:58:07 -0400

    Added table view to donors view

--- src/views/donorView/index.tsx
+++ src/views/donorView/index.tsx
@@ -1,7 +1,12 @@
 import { DonorResponse } from '@/types/persons';
-import Card from '@mui/material/Card';
-import CardContent from '@mui/material/CardContent';
-import Typography from '@mui/material/Typography';
+import {
+  Table,
+  TableBody,
+  TableCell,
+  TableContainer,
+  TableHead,
+  TableRow,
+} from '@mui/material/';
 
 interface DonorViewProps {
   donors: DonorResponse[];
@@ -9,20 +14,42 @@ interface DonorViewProps {
 
 export default function DonorView({ donors }: DonorViewProps) {
   return (
-    <div style={{ display: 'flex', flexWrap: 'wrap' }}>
-      {donors.map((person) => (
-        <Card key={person._id} style={{ margin: '10px', width: '300px' }}>
-          <CardContent>
-            <Typography variant="h5" component="h2">
-              {person.firstName} {person.lastName}
-            </Typography>
-            <Typography color="textSecondary">Email: {person.email}</Typography>
-            <Typography color="textSecondary">
-              Address: {person.address}
-            </Typography>
-          </CardContent>
-        </Card>
-      ))}
-    </div>
+    <TableContainer>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell sx={{ fontWeight: 'bold', m: 1 }}>Name</TableCell>
+            <TableCell sx={{ fontWeight: 'bold', m: 1 }}>Address</TableCell>
+            <TableCell sx={{ fontWeight: 'bold', m: 1 }} align="center">
+              City
+            </TableCell>
+            <TableCell sx={{ fontWeight: 'bold', m: 1 }} align="center">
+              State
+            </TableCell>
+            <TableCell sx={{ fontWeight: 'bold', m: 1 }} align="center">
+              Zipcode
+            </TableCell>
+            <TableCell sx={{ fontWeight: 'bold', m: 1 }} align="center">
+              Email
+            </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {donors &&
+            donors.map((donor) => (
+              <TableRow key={donor._id}>
+                <TableCell component="th" scope="row">
+                  {donor.firstName} {donor.lastName}
+                </TableCell>
+                <TableCell>{donor.address}</TableCell>
+                <TableCell align="center">{donor.city}</TableCell>
+                <TableCell align="center">{donor.state}</TableCell>
+                <TableCell align="center">{donor.zip}</TableCell>
+                <TableCell align="center">{donor.email}</TableCell>
+              </TableRow>
+            ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }
