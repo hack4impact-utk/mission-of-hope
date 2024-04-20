5ff1eba97f4ebfdf1752835f6cc872dfff88f755
TrishuPatel <trishujpatel23@gmail.com>
2024-04-20 16:05:55 -0400

    Add sign in page

--- src/app/signin/page.tsx
+++ src/app/signin/page.tsx
@@ -1,33 +1,48 @@
-import { Grid } from '@mui/material';
+import { Card, Box, Typography } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
 
 export default function signIn() {
   return (
-    <Grid
-      container
-      direction="row"
-      justifyContent="space-evenly"
-      alignItems="stretch"
-      margin={0}
-      sx={{ background: 'green', height: '100vh', overflow: 'hidden' }}
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignContent: 'center',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        position: 'absolute',
+        left: '0',
+        right: '0',
+        top: '0',
+        bottom: '0',
+      }}
     >
-      <Grid
-        sx={{ background: 'red' }}
-        xs={5}
-        justifyContent="center"
-        alignItems="center"
+      <Box>
+        <img
+          className="rounded-md"
+          alt="logo"
+          src="https://missionofhope.org/wp-content/uploads/2023/04/cropped-MOH-Logo-768x393.png"
+          width="360px"
+          height="200px"
+          style={{ marginRight: '100px' }}
+        />
+      </Box>
+      <Card
+        sx={{
+          padding: '2rem 3rem',
+          color: '#144D29',
+          background: '#ABABAB4D',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '35vw',
+        }}
       >
-        {/* <Image
-          src={
-            'https://missionofhope.org/wp-content/uploads/2023/04/cropped-MOH-Logo.png'
-          }
-          alt={'Mission of Hope Logo'}
-          fill={true}
-        ></Image> */}
-        1
-      </Grid>
-      <Grid xs={5} sx={{ background: 'blue' }}>
-        2
-      </Grid>
-    </Grid>
+        <Typography variant="h3" sx={{ fontWeight: 'bold' }}>
+          Sign in with your Google Account
+        </Typography>
+        <AccountCircleIcon sx={{ color: '#FFFFFF', fontSize: '10rem' }} />
+      </Card>
+    </Box>
   );
 }
