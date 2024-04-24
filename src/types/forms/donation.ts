5a53acd975cc4e23bb8b4fc6c53873d4af312e84
alay10 <alay10@vols.utk.edu>
2024-04-24 01:57:13 -0400

    added positve check

--- src/types/forms/donation.ts
+++ src/types/forms/donation.ts
@@ -7,13 +7,13 @@ export const zDonationFormData = z.object({
   donorAddress: z.string().min(1, { message: 'Required' }),
   donorCity: z.string().min(1, { message: 'Required' }),
   donorState: z.string().min(1, { message: 'Required' }),
-  donorzip: z.string().min(1, { message: 'Required' }),
+  donorzip: z.number().positive().min(1, { message: 'Required' }),
   donationDate: z.date(),
   category: z.string().min(1, { message: 'Required' }),
   donatedItemName: z.string().min(1, { message: 'Required' }),
-  quantity: z.number().min(1, { message: 'Required' }),
+  quantity: z.number().positive().min(1, { message: 'Required' }),
   newOrUsed: z.string().min(1, { message: 'Required' }),
-  price: z.number().min(1, { message: 'Required' }),
+  price: z.number().positive().min(1, { message: 'Required' }),
   user: z.string().min(1, { message: 'Required' }),
 });
 
--- src/types/forms/item.ts
+++ src/types/forms/item.ts
@@ -3,8 +3,8 @@ import { z } from 'zod';
 export const zItemFormData = z.object({
   name: z.string().min(1, { message: 'Required' }),
   category: z.string().min(1, { message: 'Required' }),
-  high: z.number().min(1, { message: 'Required' }),
-  low: z.number().min(1, { message: 'Required' }),
+  high: z.number().positive().min(1, { message: 'Required' }),
+  low: z.number().positive().min(1, { message: 'Required' }),
 });
 
 export interface ItemFormData extends z.infer<typeof zItemFormData> {}
