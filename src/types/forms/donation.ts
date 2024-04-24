e8f4d3542c94b0264eac3ceafafa1e5ad65904a6
alay10 <alay10@vols.utk.edu>
2024-04-24 01:46:31 -0400

    Added form data for validation

---
+++ src/types/forms/donation.ts
@@ -0,0 +1,20 @@
+import { z } from 'zod';
+
+export const zDonationFormData = z.object({
+  donorFirstName: z.string().min(1, { message: 'Required' }),
+  donorLastName: z.string().min(1, { message: 'Required' }),
+  donorEmail: z.string().email().min(1, { message: 'Required' }),
+  donorAddress: z.string().min(1, { message: 'Required' }),
+  donorCity: z.string().min(1, { message: 'Required' }),
+  donorState: z.string().min(1, { message: 'Required' }),
+  donorzip: z.string().min(1, { message: 'Required' }),
+  donationDate: z.date(),
+  category: z.string().min(1, { message: 'Required' }),
+  donatedItemName: z.string().min(1, { message: 'Required' }),
+  quantity: z.number().min(1, { message: 'Required' }),
+  newOrUsed: z.string().min(1, { message: 'Required' }),
+  price: z.number().min(1, { message: 'Required' }),
+  user: z.string().min(1, { message: 'Required' }),
+});
+
+export interface DonationFormData extends z.infer<typeof zDonationFormData> {}
