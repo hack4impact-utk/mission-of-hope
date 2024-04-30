ebf4f82ac6538f58276a5e9b3cdf12d0c3dc1e9e
TrishuPatel <trishujpatel23@gmail.com>
2024-04-29 20:51:15 -0400

    Convert to view

---
+++ src/views/signInView/index.tsx
@@ -0,0 +1,50 @@
+'use client';
+
+import { Card, Box, Typography } from '@mui/material';
+import AccountCircleIcon from '@mui/icons-material/AccountCircle';
+
+export default function SignInView() {
+  return (
+    <Box
+      sx={{
+        display: 'flex',
+        flexDirection: 'row',
+        alignContent: 'center',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        position: 'absolute',
+        left: '0',
+        right: '0',
+        top: '0',
+        bottom: '0',
+      }}
+    >
+      <Box>
+        <img
+          className="rounded-md"
+          alt="logo"
+          src="https://missionofhope.org/wp-content/uploads/2023/04/cropped-MOH-Logo-768x393.png"
+          width="360px"
+          height="200px"
+          style={{ marginRight: '100px' }}
+        />
+      </Box>
+      <Card
+        sx={{
+          padding: '2rem 3rem',
+          color: '#144D29',
+          background: '#ABABAB4D',
+          display: 'flex',
+          flexDirection: 'column',
+          alignItems: 'center',
+          width: '35vw',
+        }}
+      >
+        <Typography variant="h3" sx={{ fontWeight: 'bold' }}>
+          Sign in with your Google Account
+        </Typography>
+        <AccountCircleIcon sx={{ color: '#FFFFFF', fontSize: '10rem' }} />
+      </Card>
+    </Box>
+  );
+}
