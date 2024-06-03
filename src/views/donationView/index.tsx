120e711d869f9eea51e3713cd83f13dd8e6b77eb
alay10 <alay10@vols.utk.edu>
2024-06-03 18:03:01 -0400

    can sort donations by month

--- src/views/donationView/index.tsx
+++ src/views/donationView/index.tsx
@@ -1,15 +1,43 @@
 'use client';
 import { DonationResponse } from '@/types/donation';
-import { Box, Container, IconButton, Typography } from '@mui/material';
+import {
+  Box,
+  Container,
+  Grid,
+  IconButton,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Typography,
+} from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 import { DataGrid, GridColDef, GridToolbar } from '@mui/x-data-grid';
+import { useState } from 'react';
 
 interface DonationViewProps {
   donations: DonationResponse[];
 }
 
 export default function DonationView({ donations }: DonationViewProps) {
-  const rows = donations.map((donation, index) => ({
+  const [selectedMonth, setSelectedMonth] = useState<string>(
+    (new Date().getMonth() + 1).toString() // Default to current month
+  );
+
+  // Function to handle month selection change
+  const handleMonthChange = (event: SelectChangeEvent<string>) => {
+    setSelectedMonth(event.target.value as string);
+  };
+
+  const filteredDonations = selectedMonth
+    ? donations.filter(
+        (donation) =>
+          parseInt(
+            donation.entryDate.toString().split('T')[0].split('-')[1]
+          ) === parseInt(selectedMonth)
+      )
+    : donations;
+
+  const rows = filteredDonations.map((donation, index) => ({
     id: index + 1,
     donor: `${donation.donor.firstName} ${donation.donor.lastName}`,
     quantity: donation.items.length,
@@ -58,11 +86,36 @@ export default function DonationView({ donations }: DonationViewProps) {
   return (
     <Container>
       <Box sx={{ maxWidth: '73vw', height: '78vh' }}>
-        <Box sx={{ display: 'flex', alignItems: 'center', p: 2, pl: 0 }}>
-          <Typography variant="h4" sx={{ mr: 2 }}>
-            Donation List
-          </Typography>
-        </Box>
+        <Grid
+          container
+          spacing={2}
+          sx={{ alignContent: 'center', width: '100%', p: 2, pl: 0, pr: 0 }}
+        >
+          <Grid item xs={4}>
+            <Typography variant="h4">Donation List</Typography>
+          </Grid>
+          <Grid item xs={4}></Grid>
+          <Grid item xs={4}>
+            <Select
+              fullWidth
+              value={selectedMonth}
+              onChange={handleMonthChange}
+              variant="outlined"
+              displayEmpty
+              inputProps={{ 'aria-label': 'Select month' }}
+            >
+              <MenuItem value="">All Months</MenuItem>
+              {/* Generate month options */}
+              {[...Array(12).keys()].map((month) => (
+                <MenuItem key={month} value={month + 1}>
+                  {new Date(2000, month).toLocaleString('default', {
+                    month: 'long',
+                  })}
+                </MenuItem>
+              ))}
+            </Select>
+          </Grid>
+        </Grid>
         <DataGrid
           rows={rows}
           columns={columns}
