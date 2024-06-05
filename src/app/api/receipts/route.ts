7b64015c06cbed470401c3f602a7c4ab1cd1cd22
alay10 <alay10@vols.utk.edu>
2024-06-05 13:41:23 -0400

    added generate reciept button. buggy

--- src/app/api/receipts/route.ts
+++ src/app/api/receipts/route.ts
@@ -5,7 +5,7 @@ export async function GET() {
   try {
     const result = await incrementReceiptNumber();
     const receipt_number = String(result).padStart(6, '0');
-    return NextResponse.json(receipt_number, { status: 200 });
+    return NextResponse.json({ value: receipt_number }, { status: 200 });
   } catch (error) {
     return NextResponse.json({ message: error }, { status: 500 });
   }
