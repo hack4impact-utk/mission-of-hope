024b2a476005fd980d43729f5ab8154146718c7e
djwoun <65102751+djwoun@users.noreply.github.com>
2024-10-12 19:32:04 -0400

    Fix page rerender with useEffect hook

--- src/views/donationItemView/index.tsx
+++ src/views/donationItemView/index.tsx
@@ -1,4 +1,5 @@
 'use client';
+import React, { useEffect } from 'react';
 import {
   Box,
   Chip,
@@ -75,9 +76,11 @@ export default function DonationItemView({ donations }: DonationItemProps) {
   //map the donation items to the rows
   const { searchString, searchQuery, setSearchQuery } = useSearch();
   const { selectedMonth, monthQuery, setMonthQuery } = useMonth();
-  if (monthQuery === '') {
-    setMonthQuery((new Date().getMonth() + 1).toString()); // Default to current month
-  }
+  useEffect(() => {
+    if (monthQuery === '') {
+      setMonthQuery((new Date().getMonth() + 1).toString());
+    }
+  }, []);
   // const [selectedMonth, setSelectedMonth] = useState<string>(
   //   (new Date().getMonth() + 1).toString() // Default to current month
   // );
