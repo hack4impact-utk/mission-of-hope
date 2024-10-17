8cc1172bb9023160a399e0b8d4ce5af5935eede8
Andrew Rutter <andlrutt@vols.utk.edu>
2024-10-17 19:15:01 -0400

    auth callbacks

--- src/app/api/auth/[...nextauth]/auth.ts
+++ src/app/api/auth/[...nextauth]/auth.ts
@@ -1,8 +1,9 @@
 import { MongoDBAdapter } from '@next-auth/mongodb-adapter';
 import { MongoClient } from 'mongodb';
 import { NextAuthOptions } from 'next-auth';
-import GoogleProvider from 'next-auth/providers/google';
+import GoogleProvider, { GoogleProfile } from 'next-auth/providers/google';
 import dbConnect from '@/utils/db-connect';
+import { getUserByEmail } from '@/server/actions/users';
 
 const clientPromise = dbConnect().then((mon) => {
   return mon.connection.getClient() as unknown as Promise<MongoClient>;
@@ -14,8 +15,35 @@ export const handler: NextAuthOptions = {
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID!,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+      profile(profile: GoogleProfile) {
+        return {
+          id: profile.sub,
+          name: profile.name,
+          email: profile.email,
+          image: profile.picture,
+          isAdmin: false,
+        };
+      },
     }),
   ],
+  callbacks: {
+    async session({ session, token }) {
+      if (token.data && session.user) {
+        // eslint-disable-next-line @typescript-eslint/no-explicit-any
+        session.user.isAdmin = (token.data as any).isAdmin;
+      }
+      return session;
+    },
+    async jwt({ token }) {
+      if (token.email) {
+        try {
+          const user = await getUserByEmail(token.email);
+          token.data = user;
+        } catch {}
+      }
+      return token;
+    },
+  },
   session: {
     strategy: 'jwt',
   },
