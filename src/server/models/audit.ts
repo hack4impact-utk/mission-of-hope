4d382540d7e33f352c5443f5fe1d1ae96f39bd4b
ethanhmaness <ethanhmaness@gmail.com>
2025-04-28 16:53:39 -0400

    disable audit versionkey

--- src/server/models/audit.ts
+++ src/server/models/audit.ts
@@ -1,42 +1,48 @@
 import { model, Model, models, Schema } from 'mongoose';
 import { auditActionType, auditDocumentType, AuditEntity } from '@/types/audit';
 
-const AuditSchema = new Schema({
-  actionType: {
-    type: String,
-    enum: auditActionType,
-    required: true,
+const AuditSchema = new Schema(
+  {
+    actionType: {
+      type: String,
+      enum: auditActionType,
+      required: true,
+    },
+    docType: {
+      type: String,
+      enum: auditDocumentType,
+      required: true,
+    },
+    user: {
+      type: Schema.Types.ObjectId,
+      ref: 'User',
+      required: true,
+    },
+    doc: {
+      type: Schema.Types.ObjectId,
+      refPath: 'docType',
+      required: true,
+    },
+    timestamp: {
+      type: Date,
+      required: true,
+    },
+    oldFields: {
+      type: Map,
+      of: String,
+      required: false,
+    },
+    newFields: {
+      type: Map,
+      of: String,
+      required: false,
+    },
   },
-  docType: {
-    type: String,
-    enum: auditDocumentType,
-    required: true,
-  },
-  user: {
-    type: Schema.Types.ObjectId,
-    ref: 'User',
-    required: true,
-  },
-  doc: {
-    type: Schema.Types.ObjectId,
-    refPath: 'docType',
-    required: true,
-  },
-  timestamp: {
-    type: Date,
-    required: true,
-  },
-  oldFields: {
-    type: Map,
-    of: String,
-    required: false,
-  },
-  newFields: {
-    type: Map,
-    of: String,
-    required: false,
-  },
-});
+  {
+    versionKey: false,
+    tracking: true,
+  }
+);
 
 export interface AuditDocument extends Omit<AuditEntity, '_id'>, Document {}
 
